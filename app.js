c75d691f8bb67453610d58c7d95d44b9b45f1200
beeva-davidgarcia <david.garcia@beeva.com>
2015-08-25 14:03:46 +0200

    Initial push

--- app.js
+++ app.js
@@ -31,28 +31,28 @@ var prFile=function(file){
 };
 
 var walk = function(dir, done) {
-        var results = [];
-        fs.readdir(dir, function(err, list) {
-                if (err) return done(err);
-                var i = 0;
-                (function next() {
-                        var file = list[i++];
-                        if (!file) return done(null, results);
-                        file = dir + '/' + file;
-                        fs.stat(file, function(err, stat) {
-                                if (stat && stat.isDirectory()) {
-                                        walk(file, function(err, res) {
-                                                results = results.concat(res);
-                                                next();
-                                        });
-                                } else {
-                                        if(path.extname(file)==='.mov')
-                                            results.push(file);
-                                        next();
-                                }
-                        });
-                })();
-        });
+    var results = [];
+    fs.readdir(dir, function(err, list) {
+        if (err) return done(err);
+        var i = 0;
+        (function next() {
+            var file = list[i++];
+            if (!file) return done(null, results);
+            file = dir + '/' + file;
+            fs.stat(file, function(err, stat) {
+                if (stat && stat.isDirectory()) {
+                    walk(file, function(err, res) {
+                        results = results.concat(res);
+                        next();
+                    });
+                } else {
+                    if(path.extname(file)==='.mov')
+                        results.push(file);
+                    next();
+                }
+            });
+        })();
+    });
 };
 
 walk(__dirname,function(err,results){
